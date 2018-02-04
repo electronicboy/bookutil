bcb12c1e7b7d376eb0ca331f9ef09ff9fce364f8
Shane Freeder <theboyetronic@gmail.com>
2018-02-04 23:52:50 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,58 @@
+### BookUtil
+
+Because coming up with cool names is sooooo next year....
+
+
+# Usage
+
+Basic usage example:
+```java
+public final class Booktest extends JavaPlugin implements Listener {
+
+    private BookUtil bookUtil;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        bookUtil = new BookUtil(this);
+    }
+
+
+    @EventHandler
+    public void playerInteract(PlayerInteractEvent event) {
+
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta bookMeta = (BookMeta) book.getItemMeta();
+        bookMeta.addPage("books, wooo!!");
+        book.setItemMeta(bookMeta);
+
+        bookUtil.openBook(event.getPlayer(), book);
+    }
+
+}
+
+```
+
+# How to get it?
+
+Maven!
+
+```xml
+    <repositories>
+        <repository>
+            <id>valaria</id>
+            <url>https://repo.valaria.pw/repository/maven-public/</url>
+        </repository>
+    </repositories>
+ ```
+ 
+ ```xml
+    <dependencies>
+        <dependency>
+            <groupId>pw.valaria</groupId>
+            <artifactId>bookutil</artifactId>
+            <version>1.0-SNAPSHOT</version>
+        </dependency>
+    </dependencies>
+
+```
---
+++ src/main/java/pw/valaria/bookutil/BookUtil.java
@@ -0,0 +1,65 @@
+package pw.valaria.bookutil;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.annotation.Nonnull;
+
+public class BookUtil {
+
+    private static final String BOOK_CHANNEL = "MC|BOpen";
+    private Method addChannelMethod = null;
+    private Plugin plugin;
+
+
+    /**
+     * @param plugin plugin to bind to the instance
+     */
+    public BookUtil(Plugin plugin) {
+        this.plugin = plugin;
+        plugin.getServer().getMessenger().registerOutgoingPluginChannel(plugin, BOOK_CHANNEL);
+    }
+
+    /**
+     * @param player the player to open the book to
+     * @param book   the book to open
+     */
+    public void openBook(@Nonnull Player player, @Nonnull ItemStack book) {
+        if (book.getType() != Material.WRITTEN_BOOK)
+            throw new IllegalArgumentException("Expected a written book!");
+
+        ItemStack old = player.getInventory().getItemInMainHand();
+
+        player.getInventory().setItemInMainHand(book);
+
+        if (!player.getListeningPluginChannels().contains(BOOK_CHANNEL)) {
+
+            if (addChannelMethod == null) {
+                Class<? extends Player> aClass = player.getClass();
+
+                try {
+                    addChannelMethod = aClass.getMethod("addChannel", String.class);
+                } catch (NoSuchMethodException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+
+            try {
+                addChannelMethod.invoke(player, BOOK_CHANNEL);
+            } catch (IllegalAccessException | InvocationTargetException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+        player.sendPluginMessage(plugin, BOOK_CHANNEL, new byte[]{0});
+        player.getInventory().setItemInMainHand(old);
+    }
+}
+
